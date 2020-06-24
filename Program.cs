35ce7ab91d15cc28c82b55e8997f9f541efe6419
johnny-newton <johnnewton@ualberta.ca>
2020-06-24 15:56:12 -0600

    added Toupper; to Lower, to substring

--- Program.cs
+++ Program.cs
@@ -18,8 +18,20 @@ namespace c_sharp_intro
             //Assign variable 'userName' to a value entered by the user
 
             string userName = Console.ReadLine();
-            Console.WriteLine("You have entered your name" + userName);
+            Console.WriteLine("You have entered your name " + userName);
+            //instead of using traditional concatentation using addition operators
+            // we can use string interpolation (placeholders) to embed values in a string
             Console.WriteLine("Hey {0}, you're looking great today!", userName);
+            //@link https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated
+            //Let's play with strings!
+            Console.WriteLine("makemeuppercase".ToUpper()); //convert to uppercase
+            Console.WriteLine("makemeLOWERrcase".ToLower()); //convert to lower case
+            Console.WriteLine("ABCDEFGHIJKLMNOPQRSTUVWXYZ".Substring(3, 3));
+            /*above starts with the third character (starts at 0), and the second number tells us how many letters to
+            stubstring takes two arguments: start postion and number of characters*/
+            Console.WriteLine("xyz".Length); //get the number os characters for the string
+
+
         }
     }
 }
